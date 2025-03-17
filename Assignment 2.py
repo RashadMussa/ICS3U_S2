4916d457d8b6968fa380fbba4801d4819d74680d
RashadMussa <762918@pdsb.net>
2025-03-17 14:24:53 -0400

    Update Assignment 2.py

--- Assignment 2.py
+++ Assignment 2.py
@@ -1,40 +1,43 @@
+"""
+Author : Rashad Mussa
+Student Number: 762918
+Due date : 17 March 2025
+"""
+
+"""
+Program : Number Guessing Game
+Description : A game where the user tries to guess a randomly generated number between 1 and 100 within 6 tries.
+VARIABLE DICTIONARY :
+target_number (int) = Stores the randomly generated number between 1 and 100.
+guess_count (int) = Tracks the number of guesses the user has made.
+max_tries (int) = The maximum number of guesses allowed (6).
+user_guess (int) = Stores the number input by the user on each guess.
+"""
+
+
 import random
 
 # Initialize the game
 target_number = random.randint(1, 100)  # Randomly generate a number between 1 and 100
 guess_count = 0  # Track the number of guesses
-max_guesses = 6  # Maximum number of guesses allowed
-guessed_correctly = False  # Flag to track if the user has guessed the number
-
 print("Hello! Welcome to the number guessing game!")
 print("I am thinking of a number between 1 and 100. It is your turn to guess what it is.")
 print("You have a maximum of six (6) tries.")
 
 # Loop for user guesses
-while guess_count < max_guesses and not guessed_correctly:
+while guess_count < 6:
     guess_count += 1
-    print("Guess #", guess_count, ": ", end="")
-
-    user_guess = input()  # Get user's guess as a string
-
-    # Directly attempt to convert the input to an integer
-    if 1 <= int(user_guess) <= 100:
-        user_guess = int(user_guess)
-
-        # Check if the guess is correct, too high, or too low
-        if user_guess == target_number:
-            print("You guessed right!")
-            guessed_correctly = True  # Set flag to True when the correct number is guessed
-        elif user_guess < target_number:
-            print("Higher!")
-        else:
-            print("Lower!")
+    user_guess = int(input("Guess #%d: " % guess_count))
+    
+    # Check if the guess is correct, too high, or too low
+    if user_guess > target_number:
+        print("Lower")
+    elif user_guess < target_number:
+        print("Higher!")
     else:
-        print("Please enter a number between 1 and 100.")
-        guess_count -= 1  # Reset guess count if input is out of range
+        print("You guessed right!")
+        guess_count = 7
 
-# If the user runs out of guesses or guesses correctly
-if guessed_correctly:
-    print("Congratulations! You guessed the number in", guess_count, "tries.")
-else:
-    print("Sorry: you are out of guesses! The answer was", target_number, ". Better luck next time!")
+# After the loop ends, check if the user failed to guess the number
+if user_guess != target_number:
+    print("Sorry: you are out of guesses! The answer was %d. Better luck next time!" % target_number)
